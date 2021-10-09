a6bbbd3955f64e6308fd79acd77e883495558a05
zzy <zhouhoushu@126.com>
2021-10-09 16:13:26 +0100

    add exception

--- miniwc.py
+++ miniwc.py
@@ -1,21 +1,28 @@
 def count(path):
-    with open(path, 'rb') as file:
-        # The output is linecount wordcount bytecount filepath(each separated by
-        # tab characters)
-        bytes_read = file.read()
-        line_count = bytes_read.count(b'\n')
-        word_count = len(bytes_read.split())
-        byte_count = len(bytes_read)
+    try:
+        with open(path, 'rb') as file:
+            # The output is linecount wordcount bytecount filepath(each separated by
+            # tab characters)
+            bytes_read = file.read()
+            line_count = bytes_read.count(b'\n')
+            word_count = len(bytes_read.split())
+            byte_count = len(bytes_read)
 
-        return [line_count, word_count, byte_count, path]
+            return [line_count, word_count, byte_count, path]
+
+    except FileNotFoundError:
+        print('wc: {}: open: No such file or directory'.format(path))
 
 
 if __name__ == "__main__":
     import sys
     if len(sys.argv) != 2:
-        print('Invalid input.')
+        print('We don’t handle that situation yet!')
     else:
         path = sys.argv[1]
-        counts = count(path)
-        output = "\t".join(str(count) for count in counts)
-        print(output)
+        try:
+            counts = count(path)
+            output = "\t".join(str(count) for count in counts)
+            print(output)
+        except TypeError:
+            pass
