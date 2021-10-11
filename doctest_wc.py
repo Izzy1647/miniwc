c96029984be192017b0217171b479adb1b49fdca
zzy <zhouhoushu@126.com>
2021-10-11 17:17:18 +0100

    add total line

---
+++ doctest_wc.py
@@ -0,0 +1,5 @@
+'''
+>>> import subprocess
+>>> subprocess.check_output('python3 wc.py testinputs/atest.txt', shell=True)
+'9  5  28  testinputs/atest.txt\\n'
+'''
--- wc.py
+++ wc.py
@@ -31,11 +31,15 @@ def count(path, flags):
                     # handle illegal flag
                     return ['flag error', flag]
 
+            # generate output
             res = []
-
-            for k in flag_map:
-                if count_states[k]:
-                    res.append(flag_map[k])
+            if len(flags) == 0:
+                # no flags, show'em all
+                res = [line_count, word_count, byte_count]
+            else:
+                for k in flag_map:
+                    if count_states[k]:
+                        res.append(flag_map[k])
 
             return [*res, path]
 
@@ -51,6 +55,10 @@ def generate_output(params):
 if __name__ == "__main__":
     import sys
     arguments = list(sys.argv[1:])
+    if len(arguments) == 0:
+        print('We don’t handle that situation yet!')
+        sys.exit()
+
     flags = []
     paths = []
 
@@ -62,16 +70,28 @@ if __name__ == "__main__":
             paths = arguments[index:]
             break
 
+    totals = []  # for the 'totals' line
+
     # handle each path via count() function
     for path in paths:
         try:
+            # save figures for 'totals' line
             counts = count(path, flags)
+            figures = counts[0: len(counts)-1]
+            totals.append(figures)
+
             # handle illegal flags
-            if counts[0] == 'flag error':
-                print('wc: illegal option -- {}'.format(counts[1][1:]))
-            else:
-                res = generate_output(counts)
-                print(res)
+            output = 'wc: illegal option -- {}'.format(counts[1][1:]) if counts[0] == 'flag error' else generate_output(counts)
+            print(output)
 
         except TypeError:
             pass
+
+    # total_res for the 'totals' line'
+    if len(totals) > 1:
+        total_res = [0] * len(totals[0])
+        for nums in totals:
+            for index, num in enumerate(total_res):
+                total_res[index] += nums[index]
+        total_res = [*total_res, 'total']
+        print("\t" + "\t".join(str(item) for item in total_res))
